33844b420081449ed804f90de985b4e21358d615
diogosfpinto <diogo.sfpinto@gmail.com>
2020-11-27 19:51:32 -0400

    servidor criado Node; Efetuado o download da dependencia Marko

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+//Importa a biblioteca do Mako
+require("marko/node-require");
+
 const http = require('http');
+
+const page = require("./hello");
+const port = 3000;
+
+http.createServer(function(req, resp){
+    hello.render({ name: "Marko"}, resp)
+}).listen(port);;
