bd8429cb9edf540899cc29eb3b9c99964b3b9f14
diogosfpinto <diogo.sfpinto@gmail.com>
2020-11-27 20:06:21 -0400

    substituído arquivo marko para uma página com javascript

--- hello.marko.js
+++ hello.marko.js
@@ -4,8 +4,6 @@
 var marko_template = module.exports = require("marko/src/html").t(__filename),
     marko_componentType = "/entendendomarko$1.0.0/hello.marko",
     marko_renderer = require("marko/src/runtime/components/renderer"),
-    helpers_escape_xml = require("marko/src/runtime/html/helpers/escape-xml"),
-    marko_escapeXml = helpers_escape_xml.x,
     marko_loadTag = require("marko/src/runtime/helpers/load-tag"),
     init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
     await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer")),
@@ -14,13 +12,11 @@ var marko_template = module.exports = require("marko/src/html").t(__filename),
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<!DOCTYPE html><html lang=pt-br><head><title>Marko</title><meta charset=UTF-8><meta name=viewport content=width=device-width> </head><body> <h1>Hello " +
-    marko_escapeXml(input.name) +
-    "</h1>");
+  out.w("<html><head><meta charset=utf-8><title>Botao Adição</title></head><body><h1>Incrementar </h1><button onclick=addOne()>Adicionar</button><p id=valor>0</p><script>\r\n            let valFin = 0;\r\n            function addOne() {\r\n                valFin++;\r\n                document.getElementById('valor').innerHTML = valFin;\r\n            }\r\n        </script>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "7");
+  await_reorderer_tag({}, out, __component, "8");
 
   _preferred_script_location_tag({}, out);
 
