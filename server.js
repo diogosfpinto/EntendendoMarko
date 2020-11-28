f1c05337581d24f0b3c532f895e0b34d96b20758
diogosfpinto <diogo.sfpinto@gmail.com>
2020-11-27 20:01:09 -0400

    Efetuado correção da variável que chama o método render

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //Importa a biblioteca do Mako
-require("marko/node-require");
+require("marko/node-require").install();
 
-const http = require('http');
+const http = require("http");
 
 const page = require("./hello");
 const port = 3000;
 
-http.createServer(function(req, resp){
-    hello.render({ name: "Marko"}, resp)
-}).listen(port);;
+http.createServer((req, resp) => {
+    page.render({ name: "Marko"}, resp);
+})
+.listen(port);
